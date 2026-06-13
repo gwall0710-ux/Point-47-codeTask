287ad998d4dbb69824dfe7fc66c8db2e8195f73f
gwall0710-ux <gwall0710@gmail.com>
2026-06-13 12:48:43 +0100

    Add the IRecipe interface to the Recipe class

--- Recruitment/Recipe.cs
+++ Recruitment/Recipe.cs
@@ -6,11 +6,13 @@ using System.Threading.Tasks;
 
 namespace Recruitment
 {
-    public class Recipe : RecipeBase
+    public class Recipe : RecipeBase, IRecipe
     {
         public Recipe(string code, string description)
             :base(code, description) { }
 
         public override decimal TotalWeight => Ingredients.Sum(i => i.Quantity);
+
+        public decimal CookLoss => throw new NotImplementedException();
     }
 }
